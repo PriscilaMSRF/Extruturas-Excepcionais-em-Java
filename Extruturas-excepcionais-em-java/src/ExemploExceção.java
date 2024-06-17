5a1564f92e7f08153ee6abc50aa0e8d405e32560
PriscilaMSRF <pri.moraesds@gmail.com>
2024-06-17 12:02:36 -0300

    Estruturas-excepcionais.java

--- Extruturas-excepcionais-em-java/src/ExemploExceção.java
+++ Extruturas-excepcionais-em-java/src/ExemploExceção.java
@@ -1,7 +1,17 @@
+import java.text.NumberFormat;
+import java.text.ParseException;
+
 public class ExemploExceção {
     public static void main(String[] args) {
-        Number valor= Double.valueOf("al.75");
+        Number valor;
+        try {
+            valor = NumberFormat.getInstance().parse ("al.75");
+            System.out.println(valor);
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         System.out.println(valor);
-        
+
     }
 }
